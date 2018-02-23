6491a174eb2d22522e18ecfd9c36eb47158da089
garviswbs <30781787+garviswbs@users.noreply.github.com>
2018-02-22 20:25:29 -0700

    add
    add

---
+++ js/myScript.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+    // Toggle elements using nav
+    $("#code").click(function () {
+        $('.code').toggle();
+        $('#code').toggle(css('color', 'red'));
+    });
+    $("#photo").click(function () {
+        $('.photo').toggle();
+    });
+    $("#video").click(function () {
+        $('.video').toggle();
+    });
+    $("#illustration").click(function () {
+        $('.illustration').toggle();
+    });
+    $("#audio").click(function () {
+        $('.audio').toggle();
+    });
+    $("#tech").click(function () {
+        $('.tech').toggle();
+    });
+    $("#contact").click(function () {
+        $('.contact').toggle();
+    });
+
+    // FOOTER
+
+});
