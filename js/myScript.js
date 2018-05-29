39f426109d23bcf8e654848da7c36d87bee715e8
GW <garviswbs@gmail.com>
2018-05-28 23:34:02 -0600

    pre percent change

--- js/myScript.js
+++ js/myScript.js
@@ -1,29 +1,82 @@
 $(document).ready(function () {
-
-    // Toggle elements using nav
-    $("#code").click(function () {
-        $('.code').toggle();
-        $('#code').toggle(css('color', 'red'));
+    // external js: masonry.pkgd.js
+    $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: 50,
+        gutter: 50
     });
-    $("#photo").click(function () {
-        $('.photo').toggle();
-    });
-    $("#video").click(function () {
-        $('.video').toggle();
-    });
-    $("#illustration").click(function () {
-        $('.illustration').toggle();
-    });
-    $("#audio").click(function () {
-        $('.audio').toggle();
-    });
-    $("#tech").click(function () {
-        $('.tech').toggle();
-    });
-    $("#contact").click(function () {
-        $('.contact').toggle();
-    });
-
-    // FOOTER
-
 });
+
+// var $grid = $('.grid').imagesLoaded(function () {
+//     $grid.masonry({
+//         itemSelector: '.grid-item',
+//         percentPosition: true,
+//         columnWidth: '.grid-sizer',
+//         // isFitWidth: true,
+//     });
+// });
+
+// Toggle elements using nav
+$("#code").click(function () {
+    $('.photo , .av , .illi , .tech , .about , .connect ').toggle();
+    // $('#code').toggle(css('color', 'red'));
+    $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: 50,
+        gutter: 50
+    });
+});
+
+$("#photo").click(function () {
+    $('.code , .av , .illi , .tech , .about , .connect ').toggle();
+    $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: 50,
+        gutter: 50
+    });
+});
+
+$("#av").click(function () {
+    $('.code, .photo  , .illi , .tech , .about , .connect ').toggle();
+    $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: 50,
+        gutter: 50
+    });
+});
+
+$("#illi").click(function () {
+    $('.code, .photo , .av  , .tech , .about , .connect ').toggle();
+    $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: 50,
+        gutter: 50
+    });
+});
+
+$("#tech").click(function () {
+    $('.code, .photo , .av , .illi  , .about , .connect ').toggle();
+    $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: 50,
+        gutter: 50
+    });
+});
+$("#about").click(function () {
+    $('.code, .photo , .av , .illi , .tech , .connect').toggle();
+    $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: 50,
+        gutter: 50
+    });
+});
+$("#connect").click(function () {
+    $('.code, .photo , .av , .illi , .tech , .about  ').toggle();
+    $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: 50,
+        gutter: 50
+    });
+});
+
+// FOOTER
